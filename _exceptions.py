194383c5cf9d5af712aa2b03063ed64c9c24f2b4
Shubham Kumar <shubamk99000@gmail.com>
2026-08-19 22:48:43 +0530

    Add files via upload

--- _exceptions.py
+++ _exceptions.py
@@ -1,107 +1,177 @@
-# Copyright 2017 The Abseil Authors.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#      http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
-"""Exception classes in ABSL flags library.
-
-Do NOT import this module directly. Import the flags package and use the
-aliases defined at the package level instead.
-"""
+from __future__ import annotations
 
 import sys
+from collections.abc import Generator
+from textwrap import dedent
+from typing import Any
 
-from absl.flags import _helpers
-
-
-_helpers.disclaim_module_ids.add(id(sys.modules[__name__]))
-
-
-class Error(Exception):
-  """The base class for all flags errors."""
+if sys.version_info < (3, 11):
+    from exceptiongroup import BaseExceptionGroup
 
 
-class CantOpenFlagFileError(Error):
-  """Raised when flagfile fails to open.
-
-  E.g. the file doesn't exist, or has wrong permissions.
-  """
-
-
-class DuplicateFlagError(Error):
-  """Raised if there is a flag naming conflict."""
-
-  @classmethod
-  def from_flag(cls, flagname, flag_values, other_flag_values=None):
-    """Creates a DuplicateFlagError by providing flag name and values.
-
-    Args:
-      flagname: str, the name of the flag being redefined.
-      flag_values: :class:`FlagValues`, the FlagValues instance containing the
-        first definition of flagname.
-      other_flag_values: :class:`FlagValues`, if it is not None, it should be
-        the FlagValues object where the second definition of flagname occurs.
-        If it is None, we assume that we're being called when attempting to
-        create the flag a second time, and we use the module calling this one
-        as the source of the second definition.
-
-    Returns:
-      An instance of DuplicateFlagError.
+class BrokenResourceError(Exception):
     """
-    first_module = flag_values.find_module_defining_flag(
-        flagname, default='<unknown>')
-    if other_flag_values is None:
-      second_module = _helpers.get_calling_module()
+    Raised when trying to use a resource that has been rendered unusable due to external
+    causes (e.g. a send stream whose peer has disconnected).
+    """
+
+
+class BrokenWorkerProcess(Exception):
+    """
+    Raised by :meth:`~anyio.to_process.run_sync` if the worker process terminates abruptly or
+    otherwise misbehaves.
+    """
+
+
+class BrokenWorkerInterpreter(Exception):
+    """
+    Raised by :meth:`~anyio.to_interpreter.run_sync` if an unexpected exception is
+    raised in the subinterpreter.
+    """
+
+    def __init__(self, excinfo: Any):
+        # This was adapted from concurrent.futures.interpreter.ExecutionFailed
+        msg = excinfo.formatted
+        if not msg:
+            if excinfo.type and excinfo.msg:
+                msg = f"{excinfo.type.__name__}: {excinfo.msg}"
+            else:
+                msg = excinfo.type.__name__ or excinfo.msg
+
+        super().__init__(msg)
+        self.excinfo = excinfo
+
+    def __str__(self) -> str:
+        try:
+            formatted = self.excinfo.errdisplay
+        except Exception:
+            return super().__str__()
+        else:
+            return dedent(
+                f"""
+                {super().__str__()}
+
+                Uncaught in the interpreter:
+
+                {formatted}
+                """.strip()
+            )
+
+
+class BusyResourceError(Exception):
+    """
+    Raised when two tasks are trying to read from or write to the same resource
+    concurrently.
+    """
+
+    def __init__(self, action: str):
+        super().__init__(f"Another task is already {action} this resource")
+
+
+class ClosedResourceError(Exception):
+    """Raised when trying to use a resource that has been closed."""
+
+
+class ConnectionFailed(OSError):
+    """
+    Raised when a connection attempt fails.
+
+    .. note:: This class inherits from :exc:`OSError` for backwards compatibility.
+    """
+
+
+def iterate_exceptions(
+    exception: BaseException,
+) -> Generator[BaseException, None, None]:
+    if isinstance(exception, BaseExceptionGroup):
+        for exc in exception.exceptions:
+            yield from iterate_exceptions(exc)
     else:
-      second_module = other_flag_values.find_module_defining_flag(
-          flagname, default='<unknown>')
-    flag_summary = flag_values[flagname].help
-    msg = ("The flag '%s' is defined twice. First from %s, Second from %s.  "
-           "Description from first occurrence: %s") % (
-               flagname, first_module, second_module, flag_summary)
-    return cls(msg)
+        yield exception
 
 
-class IllegalFlagValueError(Error):
-  """Raised when the flag command line argument is illegal."""
+class DelimiterNotFound(Exception):
+    """
+    Raised during
+    :meth:`~anyio.streams.buffered.BufferedByteReceiveStream.receive_until` if the
+    maximum number of bytes has been read without the delimiter being found.
+    """
+
+    def __init__(self, max_bytes: int) -> None:
+        super().__init__(
+            f"The delimiter was not found among the first {max_bytes} bytes"
+        )
 
 
-class UnrecognizedFlagError(Error):
-  """Raised when a flag is unrecognized.
-
-  Attributes:
-    flagname: str, the name of the unrecognized flag.
-    flagvalue: The value of the flag, empty if the flag is not defined.
-  """
-
-  def __init__(self, flagname, flagvalue='', suggestions=None):
-    self.flagname = flagname
-    self.flagvalue = flagvalue
-    if suggestions:
-      # Space before the question mark is intentional to not include it in the
-      # selection when copy-pasting the suggestion from (some) terminals.
-      tip = '. Did you mean: %s ?' % ', '.join(suggestions)
-    else:
-      tip = ''
-    super().__init__("Unknown command line flag '%s'%s" % (flagname, tip))
+class EndOfStream(Exception):
+    """
+    Raised when trying to read from a stream that has been closed from the other end.
+    """
 
 
-class UnparsedFlagAccessError(Error):
-  """Raised when accessing the flag value from unparsed :class:`FlagValues`."""
+class IncompleteRead(Exception):
+    """
+    Raised during
+    :meth:`~anyio.streams.buffered.BufferedByteReceiveStream.receive_exactly` or
+    :meth:`~anyio.streams.buffered.BufferedByteReceiveStream.receive_until` if the
+    connection is closed before the requested amount of bytes has been read.
+    """
+
+    def __init__(self) -> None:
+        super().__init__(
+            "The stream was closed before the read operation could be completed"
+        )
 
 
-class ValidationError(Error):
-  """Raised when flag validator constraint is not satisfied."""
+class TypedAttributeLookupError(LookupError):
+    """
+    Raised by :meth:`~anyio.TypedAttributeProvider.extra` when the given typed attribute
+    is not found and no default value has been given.
+    """
 
 
-class FlagNameConflictsWithMethodError(Error):
-  """Raised when a flag name conflicts with :class:`FlagValues` methods."""
+class WouldBlock(Exception):
+    """Raised by ``X_nowait`` functions if ``X()`` would block."""
+
+
+class NoEventLoopError(RuntimeError):
+    """
+    Raised by several functions that require an event loop to be running in the current
+    thread when there is no running event loop.
+
+    This is also raised by :func:`.from_thread.run` and :func:`.from_thread.run_sync`
+    if not calling from an AnyIO worker thread, and no ``token`` was passed.
+    """
+
+
+class RunFinishedError(RuntimeError):
+    """
+    Raised by :func:`.from_thread.run` and :func:`.from_thread.run_sync` if the event
+    loop associated with the explicitly passed token has already finished.
+    """
+
+    def __init__(self) -> None:
+        super().__init__(
+            "The event loop associated with the given token has already finished"
+        )
+
+
+class TaskFailed(Exception):
+    """
+    Raised when awaiting on, or attempting to access the return value of, a
+    :class:`.TaskHandle` that raised an exception.
+    """
+
+
+class TaskCancelled(TaskFailed):
+    """
+    Raised when awaiting on, or attempting to access the return value of, a
+    :class:`.TaskHandle` that was cancelled.
+    """
+
+
+class TaskNotFinished(Exception):
+    """
+    Raised when attempting to access the return value or exception of a
+    :class:`.TaskHandle` that is still pending completion.
+    """
