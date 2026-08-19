0b7a8cf0330a2e98e7b202c48a2ee639a96d868d
Shubham Kumar <shubamk99000@gmail.com>
2026-08-19 23:34:06 +0530

    Add files via upload

--- _utils.py
+++ _utils.py
@@ -1,242 +1,120 @@
-from __future__ import annotations
+# Adapted from https://stackoverflow.com/a/9558001/2536294
 
-import ipaddress
-import os
-import re
-import typing
-from urllib.request import getproxies
+import ast
+import functools
+import operator as op
+from dataclasses import dataclass
 
-from ._types import PrimitiveData
+from ._multiprocessing_helpers import mp
 
-if typing.TYPE_CHECKING:  # pragma: no cover
-    from ._urls import URL
+if mp is not None:
+    from .externals.loky.process_executor import _ExceptionWithTraceback
 
 
-def primitive_value_to_str(value: PrimitiveData) -> str:
+# supported operators
+operators = {
+    ast.Add: op.add,
+    ast.Sub: op.sub,
+    ast.Mult: op.mul,
+    ast.Div: op.truediv,
+    ast.FloorDiv: op.floordiv,
+    ast.Mod: op.mod,
+    ast.Pow: op.pow,
+    ast.USub: op.neg,
+}
+
+
+def eval_expr(expr):
+    """Somewhat safely evaluate an arithmetic expression.
+
+    >>> eval_expr('2*6')
+    12
+    >>> eval_expr('2**6')
+    64
+    >>> eval_expr('1 + 2*3**(4) / (6 + -7)')
+    -161.0
+
+    Raises ValueError if the expression is invalid, too long
+    or its computation involves too large values.
     """
-    Coerce a primitive data type into a string value.
+    # Restrict the length of the expression to avoid potential Python crashes
+    # as per the documentation of ast.parse.
+    max_length = 30
+    if len(expr) > max_length:
+        raise ValueError(
+            f"Expression {expr[:max_length]!r}... is too long. "
+            f"Max length is {max_length}, got {len(expr)}."
+        )
+    try:
+        return eval_(ast.parse(expr, mode="eval").body)
+    except (TypeError, SyntaxError, OverflowError, KeyError) as e:
+        raise ValueError(
+            f"{expr!r} is not a valid or supported arithmetic expression."
+        ) from e
 
-    Note that we prefer JSON-style 'true'/'false' for boolean values here.
-    """
-    if value is True:
-        return "true"
-    elif value is False:
-        return "false"
-    elif value is None:
-        return ""
-    return str(value)
 
+def limit(max_=None):
+    """Return decorator that limits allowed returned values."""
 
-def get_environment_proxies() -> dict[str, str | None]:
-    """Gets proxy information from the environment"""
-
-    # urllib.request.getproxies() falls back on System
-    # Registry and Config for proxies on Windows and macOS.
-    # We don't want to propagate non-HTTP proxies into
-    # our configuration such as 'TRAVIS_APT_PROXY'.
-    proxy_info = getproxies()
-    mounts: dict[str, str | None] = {}
-
-    for scheme in ("http", "https", "all"):
-        if proxy_info.get(scheme):
-            hostname = proxy_info[scheme]
-            mounts[f"{scheme}://"] = (
-                hostname if "://" in hostname else f"http://{hostname}"
-            )
-
-    no_proxy_hosts = [host.strip() for host in proxy_info.get("no", "").split(",")]
-    for hostname in no_proxy_hosts:
-        # See https://curl.haxx.se/libcurl/c/CURLOPT_NOPROXY.html for details
-        # on how names in `NO_PROXY` are handled.
-        if hostname == "*":
-            # If NO_PROXY=* is used or if "*" occurs as any one of the comma
-            # separated hostnames, then we should just bypass any information
-            # from HTTP_PROXY, HTTPS_PROXY, ALL_PROXY, and always ignore
-            # proxies.
-            return {}
-        elif hostname:
-            # NO_PROXY=.google.com is marked as "all://*.google.com,
-            #   which disables "www.google.com" but not "google.com"
-            # NO_PROXY=google.com is marked as "all://*google.com,
-            #   which disables "www.google.com" and "google.com".
-            #   (But not "wwwgoogle.com")
-            # NO_PROXY can include domains, IPv6, IPv4 addresses and "localhost"
-            #   NO_PROXY=example.com,::1,localhost,192.168.0.0/16
-            if "://" in hostname:
-                mounts[hostname] = None
-            elif is_ipv4_hostname(hostname):
-                mounts[f"all://{hostname}"] = None
-            elif is_ipv6_hostname(hostname):
-                mounts[f"all://[{hostname}]"] = None
-            elif hostname.lower() == "localhost":
-                mounts[f"all://{hostname}"] = None
+    def decorator(func):
+        @functools.wraps(func)
+        def wrapper(*args, **kwargs):
+            ret = func(*args, **kwargs)
+            try:
+                mag = abs(ret)
+            except TypeError:
+                pass  # not applicable
             else:
-                mounts[f"all://*{hostname}"] = None
+                if mag > max_:
+                    raise ValueError(
+                        f"Numeric literal {ret} is too large, max is {max_}."
+                    )
+            return ret
 
-    return mounts
+        return wrapper
 
-
-def to_bytes(value: str | bytes, encoding: str = "utf-8") -> bytes:
-    return value.encode(encoding) if isinstance(value, str) else value
-
-
-def to_str(value: str | bytes, encoding: str = "utf-8") -> str:
-    return value if isinstance(value, str) else value.decode(encoding)
+    return decorator
 
 
-def to_bytes_or_str(value: str, match_type_of: typing.AnyStr) -> typing.AnyStr:
-    return value if isinstance(match_type_of, str) else value.encode()
+@limit(max_=10**6)
+def eval_(node):
+    if isinstance(node, ast.Constant) and isinstance(node.value, (int, float)):
+        return node.value
+    elif isinstance(node, ast.BinOp):  # <left> <operator> <right>
+        return operators[type(node.op)](eval_(node.left), eval_(node.right))
+    elif isinstance(node, ast.UnaryOp):  # <operator> <operand> e.g., -1
+        return operators[type(node.op)](eval_(node.operand))
+    else:
+        raise TypeError(node)
 
 
-def unquote(value: str) -> str:
-    return value[1:-1] if value[0] == value[-1] == '"' else value
+@dataclass(frozen=True)
+class _Sentinel:
+    """A sentinel to mark a parameter as not explicitly set"""
+
+    default_value: object
+
+    def __repr__(self):
+        return f"default({self.default_value!r})"
 
 
-def peek_filelike_length(stream: typing.Any) -> int | None:
-    """
-    Given a file-like stream object, return its length in number of bytes
-    without reading it into memory.
-    """
-    try:
-        # Is it an actual file?
-        fd = stream.fileno()
-        # Yup, seems to be an actual file.
-        length = os.fstat(fd).st_size
-    except (AttributeError, OSError):
-        # No... Maybe it's something that supports random access, like `io.BytesIO`?
+class _TracebackCapturingWrapper:
+    """Protect function call and return error with traceback."""
+
+    def __init__(self, func):
+        self.func = func
+
+    def __call__(self, **kwargs):
         try:
-            # Assuming so, go to end of stream to figure out its length,
-            # then put it back in place.
-            offset = stream.tell()
-            length = stream.seek(0, os.SEEK_END)
-            stream.seek(offset)
-        except (AttributeError, OSError):
-            # Not even that? Sorry, we're doomed...
-            return None
-
-    return length
+            return self.func(**kwargs)
+        except BaseException as e:
+            return _ExceptionWithTraceback(e)
 
 
-class URLPattern:
-    """
-    A utility class currently used for making lookups against proxy keys...
-
-    # Wildcard matching...
-    >>> pattern = URLPattern("all://")
-    >>> pattern.matches(httpx.URL("http://example.com"))
-    True
-
-    # Witch scheme matching...
-    >>> pattern = URLPattern("https://")
-    >>> pattern.matches(httpx.URL("https://example.com"))
-    True
-    >>> pattern.matches(httpx.URL("http://example.com"))
-    False
-
-    # With domain matching...
-    >>> pattern = URLPattern("https://example.com")
-    >>> pattern.matches(httpx.URL("https://example.com"))
-    True
-    >>> pattern.matches(httpx.URL("http://example.com"))
-    False
-    >>> pattern.matches(httpx.URL("https://other.com"))
-    False
-
-    # Wildcard scheme, with domain matching...
-    >>> pattern = URLPattern("all://example.com")
-    >>> pattern.matches(httpx.URL("https://example.com"))
-    True
-    >>> pattern.matches(httpx.URL("http://example.com"))
-    True
-    >>> pattern.matches(httpx.URL("https://other.com"))
-    False
-
-    # With port matching...
-    >>> pattern = URLPattern("https://example.com:1234")
-    >>> pattern.matches(httpx.URL("https://example.com:1234"))
-    True
-    >>> pattern.matches(httpx.URL("https://example.com"))
-    False
-    """
-
-    def __init__(self, pattern: str) -> None:
-        from ._urls import URL
-
-        if pattern and ":" not in pattern:
-            raise ValueError(
-                f"Proxy keys should use proper URL forms rather "
-                f"than plain scheme strings. "
-                f'Instead of "{pattern}", use "{pattern}://"'
-            )
-
-        url = URL(pattern)
-        self.pattern = pattern
-        self.scheme = "" if url.scheme == "all" else url.scheme
-        self.host = "" if url.host == "*" else url.host
-        self.port = url.port
-        if not url.host or url.host == "*":
-            self.host_regex: typing.Pattern[str] | None = None
-        elif url.host.startswith("*."):
-            # *.example.com should match "www.example.com", but not "example.com"
-            domain = re.escape(url.host[2:])
-            self.host_regex = re.compile(f"^.+\\.{domain}$")
-        elif url.host.startswith("*"):
-            # *example.com should match "www.example.com" and "example.com"
-            domain = re.escape(url.host[1:])
-            self.host_regex = re.compile(f"^(.+\\.)?{domain}$")
-        else:
-            # example.com should match "example.com" but not "www.example.com"
-            domain = re.escape(url.host)
-            self.host_regex = re.compile(f"^{domain}$")
-
-    def matches(self, other: URL) -> bool:
-        if self.scheme and self.scheme != other.scheme:
-            return False
-        if (
-            self.host
-            and self.host_regex is not None
-            and not self.host_regex.match(other.host)
-        ):
-            return False
-        if self.port is not None and self.port != other.port:
-            return False
-        return True
-
-    @property
-    def priority(self) -> tuple[int, int, int]:
-        """
-        The priority allows URLPattern instances to be sortable, so that
-        we can match from most specific to least specific.
-        """
-        # URLs with a port should take priority over URLs without a port.
-        port_priority = 0 if self.port is not None else 1
-        # Longer hostnames should match first.
-        host_priority = -len(self.host)
-        # Longer schemes should match first.
-        scheme_priority = -len(self.scheme)
-        return (port_priority, host_priority, scheme_priority)
-
-    def __hash__(self) -> int:
-        return hash(self.pattern)
-
-    def __lt__(self, other: URLPattern) -> bool:
-        return self.priority < other.priority
-
-    def __eq__(self, other: typing.Any) -> bool:
-        return isinstance(other, URLPattern) and self.pattern == other.pattern
-
-
-def is_ipv4_hostname(hostname: str) -> bool:
-    try:
-        ipaddress.IPv4Address(hostname.split("/")[0])
-    except Exception:
-        return False
-    return True
-
-
-def is_ipv6_hostname(hostname: str) -> bool:
-    try:
-        ipaddress.IPv6Address(hostname.split("/")[0])
-    except Exception:
-        return False
-    return True
+def _retrieve_traceback_capturing_wrapped_call(out):
+    if isinstance(out, _ExceptionWithTraceback):
+        rebuild, args = out.__reduce__()
+        out = rebuild(*args)
+    if isinstance(out, BaseException):
+        raise out
+    return out
--- common.py
+++ common.py
@@ -1,238 +1,84 @@
-# This file is part of h5py, a Python interface to the HDF5 library.
-#
-# http://www.h5py.org
-#
-# Copyright 2008-2013 Andrew Collette and contributors
-#
-# License:  Standard 3-clause BSD; see "license.txt" for full license terms
-#           and contributor agreement.
+"""
+Small utilities for testing.
+"""
 
-import sys
+import gc
 import os
-import shutil
-import inspect
-import tempfile
-import subprocess
-from contextlib import contextmanager
-from functools import wraps
+import sys
+import sysconfig
 
-import numpy as np
-from numpy.lib.recfunctions import repack_fields
-import h5py
+from joblib._multiprocessing_helpers import mp
+from joblib.testing import SkipTest, skipif
 
-import unittest as ut
-
-
-# Check if non-ascii filenames are supported
-# Evidently this is the most reliable way to check
-# See also h5py issue #263 and ipython #466
-# To test for this, run the testsuite with LC_ALL=C
 try:
-    testfile, fname = tempfile.mkstemp(chr(0x03b7))
-except UnicodeError:
-    UNICODE_FILENAMES = False
-else:
-    UNICODE_FILENAMES = True
-    os.close(testfile)
-    os.unlink(fname)
-    del fname
-    del testfile
+    import lz4
+except ImportError:
+    lz4 = None
+
+# TODO straight removal since in joblib.test.common?
+IS_PYPY = hasattr(sys, "pypy_version_info")
+IS_GIL_DISABLED = (
+    sysconfig.get_config_var("Py_GIL_DISABLED") and not sys._is_gil_enabled()
+)
+
+# A decorator to run tests only when numpy is available
+try:
+    import numpy as np
+
+    def with_numpy(func):
+        """A decorator to skip tests requiring numpy."""
+        return func
+
+except ImportError:
+
+    def with_numpy(func):
+        """A decorator to skip tests requiring numpy."""
+
+        def my_func():
+            raise SkipTest("Test requires numpy")
+
+        return my_func
+
+    np = None
+
+# TODO: Turn this back on after refactoring yield based tests in test_hashing
+# with_numpy = skipif(not np, reason='Test requires numpy.')
+
+# we use memory_profiler library for memory consumption checks
+try:
+    from memory_profiler import memory_usage
+
+    def with_memory_profiler(func):
+        """A decorator to skip tests requiring memory_profiler."""
+        return func
+
+    def memory_used(func, *args, **kwargs):
+        """Compute memory usage when executing func."""
+        gc.collect()
+        mem_use = memory_usage((func, args, kwargs), interval=0.001)
+        return max(mem_use) - min(mem_use)
+
+except ImportError:
+
+    def with_memory_profiler(func):
+        """A decorator to skip tests requiring memory_profiler."""
+
+        def dummy_func():
+            raise SkipTest("Test requires memory_profiler.")
+
+        return dummy_func
+
+    memory_usage = memory_used = None
 
 
-class TestCase(ut.TestCase):
-
-    """
-        Base class for unit tests.
-    """
-
-    @classmethod
-    def setUpClass(cls):
-        cls.tempdir = tempfile.mkdtemp(prefix='h5py-test_')
-
-    @classmethod
-    def tearDownClass(cls):
-        shutil.rmtree(cls.tempdir)
-
-    def mktemp(self, suffix='.hdf5', prefix='', dir=None):
-        if dir is None:
-            dir = self.tempdir
-        return tempfile.mktemp(suffix, prefix, dir=dir)
-
-    def mktemp_mpi(self, comm=None, suffix='.hdf5', prefix='', dir=None):
-        if comm is None:
-            from mpi4py import MPI
-            comm = MPI.COMM_WORLD
-        fname = None
-        if comm.Get_rank() == 0:
-            fname = self.mktemp(suffix, prefix, dir)
-        fname = comm.bcast(fname, 0)
-        return fname
-
-    def setUp(self):
-        self.f = h5py.File(self.mktemp(), 'w')
-
-    def tearDown(self):
-        try:
-            if self.f:
-                self.f.close()
-        except:
-            pass
-
-    def assertSameElements(self, a, b):
-        for x in a:
-            match = False
-            for y in b:
-                if x == y:
-                    match = True
-            if not match:
-                raise AssertionError("Item '%s' appears in a but not b" % x)
-
-        for x in b:
-            match = False
-            for y in a:
-                if x == y:
-                    match = True
-            if not match:
-                raise AssertionError("Item '%s' appears in b but not a" % x)
-
-    def assertArrayEqual(self, dset, arr, message=None, precision=None, check_alignment=True):
-        """ Make sure dset and arr have the same shape, dtype and contents, to
-            within the given precision, optionally ignoring differences in dtype alignment.
-
-            Note that dset may be a NumPy array or an HDF5 dataset.
-        """
-        if precision is None:
-            precision = 1e-5
-        if message is None:
-            message = ''
-        else:
-            message = ' (%s)' % message
-
-        if np.isscalar(dset) or np.isscalar(arr):
-            assert np.isscalar(dset) and np.isscalar(arr), \
-                'Scalar/array mismatch ("%r" vs "%r")%s' % (dset, arr, message)
-            dset = np.asarray(dset)
-            arr = np.asarray(arr)
-
-        assert dset.shape == arr.shape, \
-            "Shape mismatch (%s vs %s)%s" % (dset.shape, arr.shape, message)
-        if dset.dtype != arr.dtype:
-            if check_alignment:
-                normalized_dset_dtype = dset.dtype
-                normalized_arr_dtype = arr.dtype
-            else:
-                normalized_dset_dtype = repack_fields(dset.dtype)
-                normalized_arr_dtype = repack_fields(arr.dtype)
-
-            assert normalized_dset_dtype == normalized_arr_dtype, \
-                "Dtype mismatch (%s vs %s)%s" % (normalized_dset_dtype, normalized_arr_dtype, message)
-
-            if not check_alignment:
-                if normalized_dset_dtype != dset.dtype:
-                    dset = repack_fields(np.asarray(dset))
-                if normalized_arr_dtype != arr.dtype:
-                    arr = repack_fields(np.asarray(arr))
-
-        if arr.dtype.names is not None:
-            for n in arr.dtype.names:
-                message = '[FIELD %s] %s' % (n, message)
-                self.assertArrayEqual(dset[n], arr[n], message=message, precision=precision, check_alignment=check_alignment)
-        elif arr.dtype.kind in ('i', 'f'):
-            assert np.all(np.abs(dset[...] - arr[...]) < precision), \
-                "Arrays differ by more than %.3f%s" % (precision, message)
-        elif arr.dtype.kind == 'O':
-            for v1, v2 in zip(dset.flat, arr.flat):
-                self.assertArrayEqual(v1, v2, message=message, precision=precision, check_alignment=check_alignment)
-        else:
-            assert np.all(dset[...] == arr[...]), \
-                "Arrays are not equal (dtype %s) %s" % (arr.dtype.str, message)
-
-    def assertNumpyBehavior(self, dset, arr, s, skip_fast_reader=False):
-        """ Apply slicing arguments "s" to both dset and arr.
-
-        Succeeds if the results of the slicing are identical, or the
-        exception raised is of the same type for both.
-
-        "arr" must be a Numpy array; "dset" may be a NumPy array or dataset.
-        """
-        exc = None
-        try:
-            arr_result = arr[s]
-        except Exception as e:
-            exc = type(e)
-
-        s_fast = s if isinstance(s, tuple) else (s,)
-
-        if exc is None:
-            self.assertArrayEqual(dset[s], arr_result)
-
-            if not skip_fast_reader:
-                self.assertArrayEqual(
-                    dset._fast_reader.read(s_fast),
-                    arr_result,
-                )
-        else:
-            with self.assertRaises(exc):
-                dset[s]
-
-            if not skip_fast_reader:
-                with self.assertRaises(exc):
-                    dset._fast_reader.read(s_fast)
-
-NUMPY_RELEASE_VERSION = tuple([int(i) for i in np.__version__.split(".")[0:2]])
-
-@contextmanager
-def closed_tempfile(suffix='', text=None):
-    """
-    Context manager which yields the path to a closed temporary file with the
-    suffix `suffix`. The file will be deleted on exiting the context. An
-    additional argument `text` can be provided to have the file contain `text`.
-    """
-    with tempfile.NamedTemporaryFile(
-        'w+t', suffix=suffix, delete=False
-    ) as test_file:
-        file_name = test_file.name
-        if text is not None:
-            test_file.write(text)
-            test_file.flush()
-    yield file_name
-    shutil.rmtree(file_name, ignore_errors=True)
+with_multiprocessing = skipif(mp is None, reason="Needs multiprocessing to run.")
 
 
-def insubprocess(f):
-    """Runs a test in its own subprocess"""
-    @wraps(f)
-    def wrapper(request, *args, **kwargs):
-        curr_test = inspect.getsourcefile(f) + "::" + request.node.name
-        # get block around test name
-        insub = "IN_SUBPROCESS_" + curr_test
-        for c in "/\\,:.":
-            insub = insub.replace(c, "_")
-        defined = os.environ.get(insub, None)
-        # fork process
-        if defined:
-            return f(request, *args, **kwargs)
-        else:
-            os.environ[insub] = '1'
-            env = os.environ.copy()
-            env[insub] = '1'
-            env.update(getattr(f, 'subproc_env', {}))
+with_dev_shm = skipif(
+    not os.path.exists("/dev/shm"),
+    reason="This test requires a large /dev/shm shared memory fs.",
+)
 
-            with closed_tempfile() as stdout:
-                with open(stdout, 'w+t') as fh:
-                    rtn = subprocess.call([sys.executable, '-m', 'pytest', curr_test],
-                                          stdout=fh, stderr=fh, env=env)
-                with open(stdout, 'rt') as fh:
-                    out = fh.read()
+with_lz4 = skipif(lz4 is None, reason="Needs lz4 compression to run")
 
-            assert rtn == 0, "\n" + out
-    return wrapper
-
-
-def subproc_env(d):
-    """Set environment variables for the @insubprocess decorator"""
-    def decorator(f):
-        f.subproc_env = d
-        return f
-
-    return decorator
+without_lz4 = skipif(lz4 is not None, reason="Needs lz4 not being installed to run")
